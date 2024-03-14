5865ba8b5ee0445f2d5a9a3445ab8d6e3d092cba
Arsenii Zhelobetskyi <arsbrotherplay@gmail.com>
2024-03-14 18:58:25 +0200

    shared memory

--- second-program.c
+++ second-program.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
-    int i;
-    printf("Я є другою програмою\n");
+int main(int argc, char *argv[], char *envp[]) {
+    printf("Я є другою програмою(%s) \n", argv[1]);
+    for (int i=0; envp[i]!=NULL; i++)
+        printf("%s\n", envp[i]);
     return 0;
 }
